78e0d5e534e729ef01d235f565849b0d8a5d1500
Anthony J Simon <zixradoom@gmail.com>
2017-01-30 09:56:11 -0500

    add a constructor
    Add the constructor for the SceneEvent and add proper type and null checking and validation of the input arguments

--- bcc-light-controller-core/src/main/java/com/s2d/bcc/light/controller/core/SceneEvent.java
+++ bcc-light-controller-core/src/main/java/com/s2d/bcc/light/controller/core/SceneEvent.java
@@ -1,7 +1,10 @@
 package com.s2d.bcc.light.controller.core;
 
+import java.util.Objects;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,15 +15,42 @@ import java.util.concurrent.TimeUnit;
  * @author Zixradoom
  *
  */
-public class SceneEvent
+public final class SceneEvent
 {
-  private Map < Light, Boolean > lightMap;
-  private long time;
-  private TimeUnit unit;
+  private final Map < Light, Boolean > lightMap;
+  private final long time;
+  private final TimeUnit unit;
   
+  /**
+   * Create a SceneEvent with the specified {@link Light}s on or off. The map will have entries
+   * with null keys or null values filtered out. Only the Lights specified in the map will be
+   * effected all others will retain their current state. The time duration will not be checked
+   * for validity, the behavior is of the time is defined by
+   * {@link java.util.concurrent.ScheduledExecutorService#schedule(java.util.concurrent.Callable,long,java.util.concurrent.TimeUnit)}
+   *
+   * @param lightMap the lighting state to update in the {@link LightController} driver
+   * @param time the duration to wait before execution of this {@link SceneEvent}
+   * @param unit the {@link TimeUnit} of the delay
+   */
+  public SceneEvent ( Map < Light, Boolean > lightMap, long time, TimeUnit unit )
+  {
+    time = time;
+    unit = Objects.requireNonNull ( unit );
+    Map < Light, Boolean > temp = new HashMap < Light, Boolean > ();
+    for ( Entry < Light, Boolean > entry : lightMap.entrySet () )
+    {
+      if ( entry.getKey () != null && entry.getValue () != null )
+        temp.put ( entry.getKey (), entry.getValue () );
+    }
+    this.lightMap = Collections.unmodifiableMap ( temp );
+  }
+  
+  /**
+   * return a map containing the lights to be set and the state to set them to.
+   */
   public Map < Light, Boolean > getLightMap ()
   {
-    return Collections.unmodifiableMap ( lightMap );
+    return lightMap;
   }
   
   /**
